7c68aff0f139568941d43df58c8fd2352f8c1724
SL <38764885+newdisc@users.noreply.github.com>
2020-11-12 19:46:40 +0530

    Allow job to run

--- src/main/java/nd/sched/web/JobController.java
+++ src/main/java/nd/sched/web/JobController.java
@@ -2,6 +2,8 @@ package nd.sched.web;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -10,10 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import nd.sched.job.IJobExecutor;
+import nd.sched.job.IJobExecutor.JobReturn;
 import nd.sched.job.factory.JobFactory;
+import nd.sched.job.service.AsyncExecutorFacade;
 
 @RestController
 @RequestMapping("api/job")
@@ -21,6 +26,8 @@ public class JobController {
     private static final Logger logger = LoggerFactory.getLogger(JobController.class);
     @Autowired
     JobFactory jobFactory;
+    @Autowired
+    private AsyncExecutorFacade executorFacade;
 
     @GetMapping("list")
     public List<? extends IJobExecutor> getJobExecutors() {
@@ -37,5 +44,17 @@ public class JobController {
     @GetMapping("logs/{job}")
     public List<String> getJobLogs(@PathVariable String job) {
         return jobFactory.getJobLogs(job);
+    }
+    
+    @GetMapping("run")
+    public JobReturn runJob(@RequestParam final String jobName, 
+    		@RequestParam(required = false) String arguments) 
+    		throws InterruptedException, ExecutionException {
+    	if (null == arguments) {
+    		arguments = "";
+    	}
+        logger.info("Run Job: {}", jobName);
+    	CompletableFuture<JobReturn> execJob = executorFacade.execute("ManualRun", jobName, arguments);
+    	return  execJob.get();
     }
 }
